08ec52f4f8c2c870bfb3268189adf6a546ecaa1b
~wdmvv <78714307+wdmvv@users.noreply.github.com>
2022-02-05 15:15:35 +0000

    flags
    added flags

--- minstall.sh
+++ minstall.sh
@@ -1,3 +1,45 @@
-read -p "Mindustry version: " ver;
-rm -f Mindustry.jar;
-wget -q --show-progress https://github.com/Anuken/Mindustry/releases/download/v"$ver"/Mindustry.jar;
+#!/bin/bash
+
+cfgsetup()
+{
+	cd ~;
+	touch .mincfg;
+	printf "~/Desktop" > .mincfg;
+}
+
+hflag()
+{
+	printf "Script that allows you to download any Mindustry version. Flags:\n\t -p - change download path and run\n\t -h - display this message\n\t -d - display download path\nUsage:\n\t./minstall.sh -p [yourpathhere]\n"
+}
+
+dflag()
+{
+	cd ~;
+	echo $(sed '1!d' .mincfg)
+}
+
+main()
+{
+	cd ~;
+	read -p "Mindustry version: " ver;
+	path=$(sed '1!d' .mincfg)
+	cd $path;
+	wget -q --show-progress https://github.com/Anuken/Mindustry/releases/download/v"$ver"/Mindustry.jar;
+	echo "Downloaded Mindustry v"$ver" at "$path""
+}
+
+while getopts "p:hd" opt; do
+	case $opt in
+		p) {
+			if ! [ -f ~/.mincfg ];
+			then cfgsetup
+			fi
+			cd ~;
+			echo "$OPTARG" > .mincfg;
+			} 
+			main ;;
+		h) hflag ;;
+		d) dflag ;;
+		*) main
+	esac
+done
