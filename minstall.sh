9e919cab9562a9e17fdf56c91b992547c048ea0e
~wdmvv <78714307+wdmvv@users.noreply.github.com>
2022-02-05 03:30:24 +0000

    :skull:

--- minstall.sh
+++ minstall.sh
@@ -1,3 +1,3 @@
 read -p "Mindustry version: " ver;
 rm -f Mindustry.jar;
-wget -q --show-progress https://github.com/Anuken/Mindustry/releases/download/v"$ver"/Mindustry.jar -o Mindustryv"$ver".jar;
+wget -q --show-progress https://github.com/Anuken/Mindustry/releases/download/v"$ver"/Mindustry.jar;
